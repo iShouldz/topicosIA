cd2c7d9cf9e91773ceae7c9430fb7a93606effe4
Pedro Souza <pedrosport10@gmail.com>
2023-11-20 20:18:50 -0300

    PIBIC: Adicionando script para splitar o CSV

---
+++ csvSplit.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def split_csv(input_file, chunk_size, output_prefix):
+    df = pd.read_csv(input_file)
+
+    chunks = [df[i:i + chunk_size] for i in range(0, df.shape[0], chunk_size)]
+    for i, chunk in enumerate(chunks):
+        output_file = f"{output_prefix}_{i + 1}.csv"
+        chunk.to_csv(output_file, index=False)
+        print(f"Arquivo {output_file} criado com sucesso.")
+
+
+input_csv = 'csv/hashRepoURL.csv'
+chunk_size = 5000
+output_prefix = 'csv/split5000/CSV5000.csv'
+
+split_csv(input_csv, chunk_size, output_prefix)
