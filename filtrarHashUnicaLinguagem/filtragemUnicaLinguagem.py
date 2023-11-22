ab9a00e669f5874738e58723192d7785d8d80660
Pedro Souza <pedrosport10@gmail.com>
2023-11-21 21:41:26 -0300

    PIBIC: Script para remover o Markdown do CSV

---
+++ filtrarHashUnicaLinguagem/filtragemUnicaLinguagem.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Ler o arquivo CSV
+df = pd.read_csv('../csv/ListagemUsernames.csv')
+
+# Filtrar as linhas que não contêm 'Markdown' na coluna 'linguagem_programacao'
+df_filtrado = df[df['linguagem_programacao'] != 'Markdown']
+
+# Salvar o novo DataFrame em um novo arquivo CSV
+df_filtrado.to_csv('CSVOutMarkdown.csv', index=False)
